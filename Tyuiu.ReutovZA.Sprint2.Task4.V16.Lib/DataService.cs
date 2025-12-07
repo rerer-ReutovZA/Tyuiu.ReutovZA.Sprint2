c92dff549141658dc53f8539438b6434b4df06c5
Захар Реутов <reutovz809@gmail.com>
2025-12-07 19:04:23 +0500

    Task4fix

--- Tyuiu.ReutovZA.Sprint2.Task4.V16.Lib/DataService.cs
+++ Tyuiu.ReutovZA.Sprint2.Task4.V16.Lib/DataService.cs
@@ -1,5 +1,10 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint2;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
 
 namespace Tyuiu.ReutovZA.Sprint2.Task4.V16.Lib
 {
@@ -7,11 +12,7 @@ namespace Tyuiu.ReutovZA.Sprint2.Task4.V16.Lib
     {
         public double Calculate(double x, double y)
         {
-            double z = 0;
-
-            
-            z = (x == 2 && y > 2) ? Math.Pow((1 + 1 / y), x) : (y - 1 / x);
-
+            double z = x * 2 < y * 2 ? Math.Pow((1 + (1 / Math.Pow(y, 2))), x) : y - (1 / (Math.Pow(x, 2)));
             return Math.Round(z, 3);
         }
     }
