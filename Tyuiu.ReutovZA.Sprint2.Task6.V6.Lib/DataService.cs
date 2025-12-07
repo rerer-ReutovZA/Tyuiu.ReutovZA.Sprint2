3f4982eff26c14da991b58762e4082a647f81da1
Захар Реутов <reutovz809@gmail.com>
2025-12-07 19:41:35 +0500

    Task6

---
+++ Tyuiu.ReutovZA.Sprint2.Task6.V6.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.ReutovZA.Sprint2.Task6.V5.Lib
+{
+    public class DataService : ISprint2Task6V5
+    {
+        public string FindCardValue(int value)
+        {
+            string cardName = value switch
+            {
+                6 => "шестерка",
+                7 => "семерка",
+                8 => "восьмерка",
+                9 => "девятка",
+                10 => "десятка",
+                11 => "валет",
+                12 => "дама",
+                13 => "король",
+                14 => "туз",
+                _ => throw new ArgumentException($"Некорректное значение карты: {value}. Допустимый диапазон: 6-14")
+            };
+
+            return cardName;
+        }
+    }
+}
