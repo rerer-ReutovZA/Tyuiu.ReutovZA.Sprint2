b7f0f5bbd66345f456e499d103cbb3060f50e065
Захар Реутов <reutovz809@gmail.com>
2025-12-07 19:14:59 +0500

    task5

---
+++ Tyuiu.ReutovZA.Sprint2.Task5.V3.Lib/DataService.cs
@@ -0,0 +1,39 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.ReutovZA.Sprint2.Task5.V3.Lib
+{
+    public class DataService : ISprint2Task5V3
+    {
+        public string FindDayName(int value)
+        {
+            string res;
+            switch (value)
+            {
+                case 1:
+                    res = "понедельник";
+                    break;
+                case 2:
+                    res = "вторник";
+                    break;
+                case 3:
+                    res = "среда";
+                    break;
+                case 4:
+                    res = "четверг";
+                    break;
+                case 5:
+                    res = "пятница";
+                    break;
+                case 6:
+                    res = "суббота";
+                    break;
+                case 7:
+                    res = "воскресенье";
+                    break;
+                default:
+                    throw new ArgumentException($"Число должно быть от 1 до 7. Значение {value}");
+            }
+
+            return res;
+        }
+    }
+}
